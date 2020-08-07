f94bb4b827c7edff9f6c04c87596690b38ad5e02
Changxin Wan <2changsonw@tongji.edu.cn>
2020-08-07 09:49:59 -0400

    Update run.py

--- cistromego/run.py
+++ cistromego/run.py
@@ -6,12 +6,12 @@ import json
 def runCistromeGO(args):
     args = optValidate(args)
     symbol_rp_dict = calcRPscore(args.bed, args.peakn, args.assembly, args.decay)
+    json.dump(symbol_rpt_dict, open("%s_rp.json"%args.prefix, "w"))
     if not args.expr:
         res = annotIndices(symbol_rp_dict, args.assembly, True, args.max_gene_number, args.min_gene_number, args.prefix)
     else:
         from expr_combine import calcRPT
         symbol_rpt_dict = calcRPT(args.expr, args.exprinfo, symbol_rp_dict, args.assembly, args.dego, args.logfc_cut, args.fdr_cut)
-        json.dump(symbol_rpt_dict, open("%s_rp.json"%args.prefix, "w"))
         res = annotIndices(symbol_rpt_dict, args.assembly, False, args.max_gene_number, args.min_gene_number, args.prefix)
     return res
 
